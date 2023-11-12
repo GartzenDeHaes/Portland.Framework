3ec1ebcbbc5befb3deb231c3e3625872fb48e5f0
GartzenDeHaes <103427029+GartzenDeHaes@users.noreply.github.com>
2023-11-11 18:26:01 -0800

    account

---
+++ Portland.Framework/RPG/TradeSys/Account.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Portland.Collections;
+
+namespace Portland.RPG.Accounting
+{
+	public enum TransactionType
+	{
+		Credit = 1,
+		Debit = -1
+	}
+
+	public enum AccountTransactionType
+	{
+		Error = 'E',
+		AdjustCr = 'A',
+		AdjustDr = 'a',
+		Deposit = 'D',
+		Withdrawl = 'W',
+		WithdrawlAtm = 'w',
+		Fee = 'F'
+	}
+
+	public struct AccountTransaction
+	{
+		public int TransactionId;
+		public DateTime Dts;
+		public TransactionType TransactionTypeCode;
+		public AccountTransactionType LedgerTransactionTypeCode;
+		public int Amount;
+	}
+
+	public class AccountBookStatement
+	{
+		public String10 ItemId;
+		public int ClosingBalance;
+		public int TotalDebit;
+		public int TotalCredit;
+	}
+
+	public class AccountStatement
+	{
+		public int AccountId;
+		public ShortDateTime Created;
+		public Vector<AccountBookStatement> ItemStatements = new();
+	}
+
+public class Account
+	{
+		public String10 OwnerCharId;
+		public int AccountId;
+		public AsciiId4 AccountTypeCode;
+		public Vector<AccountBook> AccountBooks = new();
+
+		Dictionary<String10, int> AccountBookIdx = new();
+
+		public int GetBalance(in String10 itemId)
+		{
+			if (AccountBookIdx.TryGetValue(itemId, out var accountBookIdx))
+			{
+				return AccountBooks[accountBookIdx].Balance;
+			}
+			return 0;
+		}
+	}
+
+	public class AccountBook
+	{
+		public String10 ItemId;
+		public int Balance;
+		public int CreditBalance;
+		public int DebitBalance;
+		public Vector<AccountTransaction> Transactions = new();
+	}
+}
--- Portland.Framework/RPG/TradeSys/Ledger.cs
+++ Portland.Framework/RPG/TradeSys/Ledger.cs
@@ -11,63 +11,6 @@ using Portland.Mathmatics;
 
 namespace Portland.RPG.Accounting
 {
-	public enum TransactionType
-	{
-		Credit = 1,
-		Debit = -1
-	}
-
-	public enum AccountTransactionType
-	{
-		Error = 'E',
-		AdjustCr = 'A',
-		AdjustDr = 'a',
-		Deposit = 'D',
-		Withdrawl = 'W',
-		WithdrawlAtm = 'w',
-		Fee = 'F'
-	}
-
-	public struct AccountTransaction
-	{
-		public int TransactionId;
-		public DateTime Dts;
-		public TransactionType TransactionTypeCode;
-		public AccountTransactionType LedgerTransactionTypeCode;
-		public int Amount;
-	}
-
-	public class Account
-	{
-		public String10 OwnerCharId;
-		public int AccountId;
-		public AsciiId4 AccountTypeCode;
-		public Vector<AccountBook> AccountBooks = new();
-
-		public int Balance
-	}
-
-	public class AccountBook
-	{
-		public String10 ItemId;
-		public Vector<AccountTransaction> Transactions = new();
-	}
-
-	public class AccountBookStatement
-	{
-		public String10 ItemId;
-		public int ClosingBalance;
-		public int TotalDebit;
-		public int TotalCredit;
-	}
-
-	public class AccountStatement
-	{
-		public int AccountId;
-		public ShortDateTime Created;
-		public Vector<AccountBookStatement> ItemStatements = new();
-	}
-
 	public enum LedgerType
 	{
 		Error = 0,
